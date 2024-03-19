37730e53efeab4199c5e1a31b975b9df583db8f8
Christopher Henry <chenry@mcs.anl.gov>
2024-03-19 00:09:04 -0500

    Updating notebook and adding MSCommunity

--- grow/probcommutil.py
+++ grow/probcommutil.py
@@ -19,6 +19,7 @@ from modelseedpy.core.annotationontology import convert_to_search_role, split_ro
 from modelseedpy.core.mstemplate import MSTemplateBuilder
 from modelseedpy.core.msgenome import normalize_role
 from modelseedpy.core.msensemble import MSEnsemble
+from modelseedpy.community.mscommunity import MSCommunity
 from modelseedpy.helpers import get_template
 
 class ProbCommUtil(BaseUtil):
