52cba8ac485635db486cde288c028e814d2359f8
Freiburgermsu_Sequoia <freiburgermsu@aol.com>
2024-08-21 19:21:30 +0000

    Cliff and Zhicheng modeling

--- Cliff/cliffcommutil.py
+++ Cliff/cliffcommutil.py
@@ -3,15 +3,6 @@ import os
 from os import path
 from zipfile import ZipFile
 
-from cobrakbase.core.kbasefba import FBAModel
-from modelseedpy import AnnotationOntology, MSPackageManager, MSMedia, MSModelUtil, MSBuilder, MSATPCorrection, MSGapfill, MSGrowthPhenotype, MSGrowthPhenotypes, ModelSEEDBiochem
-# from modelseedpy.core.annotationontology import convert_to_search_role, split_role
-from modelseedpy.core.mstemplate import MSTemplateBuilder
-from modelseedpy.core.msgenome import normalize_role
-# from modelseedpy.core.msensemble import MSEnsemble
-from modelseedpy.community.mscommunity import MSCommunity
-from modelseedpy.helpers import get_template
-
 # Add the parent directory to the sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from baseutil import *
@@ -19,6 +10,16 @@ import hashlib
 import pandas as pd
 from pandas import DataFrame, read_csv, concat, set_option
 from cobra.io import write_sbml_model, read_sbml_model
+
+from cobrakbase.core.kbasefba import FBAModel
+from modelseedpy import MSPackageManager
+from modelseedpy import AnnotationOntology, MSPackageManager, MSMedia, MSModelUtil, MSBuilder, MSATPCorrection, MSGapfill, MSGrowthPhenotype, MSGrowthPhenotypes, ModelSEEDBiochem
+from modelseedpy.core.annotationontology import convert_to_search_role, split_role
+from modelseedpy.core.mstemplate import MSTemplateBuilder
+from modelseedpy.core.msgenome import normalize_role
+from modelseedpy.core.msensemble import MSEnsemble
+from modelseedpy.community.mscommunity import MSCommunity#, build_from_species_models
+from modelseedpy.helpers import get_template
 
 
 
