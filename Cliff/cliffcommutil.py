5bfd34f92fba8cc54bb85e782a249345018a5b28
Freiburgermsu_Sequoia <freiburgermsu@aol.com>
2024-08-27 21:41:36 +0000

    recreated community models

--- Cliff/cliffcommutil.py
+++ Cliff/cliffcommutil.py
@@ -13,12 +13,13 @@ from cobra.io import write_sbml_model, read_sbml_model
 
 from cobrakbase.core.kbasefba import FBAModel
 from modelseedpy import MSPackageManager
-from modelseedpy import AnnotationOntology, MSPackageManager, MSMedia, MSModelUtil, MSBuilder, MSATPCorrection, MSGapfill, MSGrowthPhenotype, MSGrowthPhenotypes, ModelSEEDBiochem
-from modelseedpy.core.annotationontology import convert_to_search_role, split_role
+# from modelseedpy import AnnotationOntology, MSPackageManager, MSMedia, MSModelUtil, MSBuilder, MSATPCorrection, MSGapfill, MSGrowthPhenotype, MSGrowthPhenotypes, ModelSEEDBiochem
+# from modelseedpy.core.annotationontology import convert_to_search_role, split_role
 from modelseedpy.core.mstemplate import MSTemplateBuilder
 from modelseedpy.core.msgenome import normalize_role
-from modelseedpy.core.msensemble import MSEnsemble
-from modelseedpy.community.mscommunity import MSCommunity#, build_from_species_models
+# from modelseedpy.core.msensemble import MSEnsemble
+from modelseedpy.community.mscommunity import MSCommunity
+from modelseedpy.community import build_from_species_models
 from modelseedpy.helpers import get_template
 
 
