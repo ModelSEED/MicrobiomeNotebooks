d4092f65de5f5cc6f5eeb9addac5ec37ed8297f7
Freiburgermsu <Freiburgermsu@aol.com>
2025-09-30 18:36:26 -0500

    PATRIC 16S and new sludge data

--- NewWesternDiet/agoracommutil.py
+++ NewWesternDiet/agoracommutil.py
@@ -19,13 +19,13 @@ from modelseedpy.core.annotationontology import convert_to_search_role, split_ro
 from modelseedpy.core.mstemplate import MSTemplateBuilder
 from modelseedpy.core.msgenome import normalize_role
 from modelseedpy.core.msensemble import MSEnsemble
-from modelseedpy.community.mscommunity import MSCommunity
+from mscommunity import MSCommunity
 from modelseedpy.helpers import get_template
 
 class AGORACommUtil(BaseUtil):
     def __init__(self):
         BaseUtil.__init__(self,"NewWesternDiet")
-        self.msseedrecon()
+        # self.msseedrecon()
     
     def create_phenotypeset_from_compounds(
         self,
